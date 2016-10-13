f45f6874c47d4bf6f06ca97337356676c49296bf
Mateus Marquezini <mateusmarquezini01@gmail.com>
2016-10-13 19:53:24 -0300

    Novas telas no sistema e melhorias na interface

---
+++ app/src/main/java/com/br/apptopicos/BoasVindasActivity.java
@@ -0,0 +1,13 @@
+package com.br.apptopicos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoasVindasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boas_vindas);
+    }
+}
--- app/src/main/java/com/br/apptopicos/LoginActivity.java
+++ app/src/main/java/com/br/apptopicos/LoginActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,5 +23,38 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intentCadastrarUsuario);
             }
         });
+
+        Button botaoEntrar = (Button) findViewById(R.id.btnEntrar);
+        botaoEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // login mockado apenas para teste
+                final EditText nomeUsuario = (EditText) findViewById(R.id.loginNomeUsuario);
+                final EditText senhaUsuario = (EditText) findViewById(R.id.loginSenhaUsuario);
+
+                final String nomeDigitado = nomeUsuario.getText().toString();
+                final String senhaDigitada = senhaUsuario.getText().toString();
+
+                if (nomeDigitado.length() >= 5 && senhaDigitada.length() >= 5
+                        && "admin".equals(nomeDigitado)
+                        && "admin".equals(senhaDigitada)) {
+                    entrar();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login inválido, tente novamente!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Intent intentEntrar = new Intent(LoginActivity.this, BoasVindasActivity.class);
+                startActivity(intentEntrar);
+                finish();
+            }
+        });
+
+
+    }
+
+
+    private void entrar() {
+
     }
 }
